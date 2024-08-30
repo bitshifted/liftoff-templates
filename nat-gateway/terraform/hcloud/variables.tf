858d50eb691ece9df1d34eb20199c42be512a189
Vladimir Djurovic <5014293+vdjurovic@users.noreply.github.com>
2024-08-30 11:17:07 +0200

    fix formatting

--- nat-gateway/terraform/hcloud/variables.tf
+++ nat-gateway/terraform/hcloud/variables.tf
@@ -1,17 +1,17 @@
 variable "private_networks" {
   type = set(object({
-    name = string
+    name     = string
     ip_range = string
   }))
 }
 
 variable "gateways" {
   type = set(object({
-    name = string
-    os_image = optional(string, "ubuntu-24.04")
+    name          = string
+    os_image      = optional(string, "ubuntu-24.04")
     instance_type = optional(string, "cx22")
-    location = string
-    networks = set(string)
-    labels = optional(map(string))
+    location      = string
+    networks      = set(string)
+    labels        = optional(map(string))
   }))
 }
